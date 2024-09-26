4e21ea210f961ba6397dd8bc2326e7584e619564
jose <josegfernandes03@gmail.com>
2024-09-26 13:11:47 -0300

    alterações no crud

--- app/controllers/video_controller.py
+++ app/controllers/video_controller.py
@@ -1,19 +1,27 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from app.models.entities import Video
+from app.models.entities import Video, Category
 from http import HTTPStatus
 
 from app.schemas.request import VideoSchema
 
 
 def create_video_controller(db: Session, video: VideoSchema):
+    db_category = db.query(Category).filter(Category.id == video.category_id).first()
+
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Category not found")
+
     db_video = Video(
         title=video.title,
         url=video.url,
         description=video.description,
         category_id=video.category_id,
     )
+
     db.add(db_video)
     db.commit()
+    db.refresh(db_video)
 
     return db_video
 
@@ -26,6 +34,10 @@ def read_video_controller(db: Session, video_id: int):
     query = db.query(Video).filter(Video.id == video_id).first()
 
     if not query:
-        return {"msg": "Video not found", "status_code": HTTPStatus.NOT_FOUND}
+        raise HTTPException(status_code=404, detail="Video not found")
 
     return query
+
+
+def list_categories_controller(db: Session):
+    return db.query(Category).all()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from app.controllers.video_controller import create_video_controller, list_videos_controller, read_video_controller
+from starlette.middleware.cors import CORSMiddleware
+
+from app.controllers.video_controller import create_video_controller, list_videos_controller, read_video_controller, \
+    list_categories_controller
 from app.internal_modules.database import engine, Base, session, get_db
 from app.models.entities import User
 from app.schemas.request import VideoSchema
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/videos/", response_model=VideoSchema)
@@ -22,3 +33,8 @@ def read_videos(db: Session = Depends(get_db)):
 @app.get("/videos/{video_id}")
 def read_video(video_id: int, db: Session = Depends(get_db)):
     return read_video_controller(db, video_id)
+
+
+@app.get("/categories/")
+def get_cateogories(db: Session = Depends(get_db)):
+    return list_categories_controller(db)
